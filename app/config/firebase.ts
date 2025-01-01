cd61521ebd4b20111f453fd27f734d74fff96aca
Charan Sri Teja <68527259+itscharanteja@users.noreply.github.com>
2025-01-01 16:54:16 +0100

    Added the firebase configuration in environment variables

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,16 +1,26 @@
 import { initializeApp } from "firebase/app";
+//@ts-ignore
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBi9R4Krp-QQrJR3GGiSWl_OoibjsxPZHs",
-  authDomain: "expensehive2.firebaseapp.com",
-  projectId: "expensehive2",
-  storageBucket: "expensehive2.firebasestorage.app",
-  messagingSenderId: "198416127927",
-  appId: "1:198416127927:web:5e5da475fcc2d291bd7a32",
-  measurementId: "G-8BX9BSXYRG",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
